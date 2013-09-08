356c192450b7ccdddd3f4e35beb33d10b317e4e7
Robert DeLuca <robertd@izea.com>
2013-09-08 19:21:41 -0400

    Starting the long journey!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MondernMustangs::Application.config.secret_key_base = 'd3f18bc8b0dab4ec993212de4a094dae4d2d228f3741dcf1a0982b157c588e7ad7f5d594c81d7a18093a20b6e5afbb9fa2d3df666acb97896442d7d5880f8ea4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MondernMustangs::Application.config.session_store :cookie_store, key: '_MondernMustangs_session'
