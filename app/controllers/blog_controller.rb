2945fc9d0daaca7e1ac21f46a3b87c2e3c83d400
Robert DeLuca <robertd@izea.com>
2013-09-14 02:03:41 -0400

    Set up blog and got it working pretty well with titles and what not

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,14 @@
 class BlogController < ApplicationController
 
+  # Make all the blog stuff use the same layout
+  layout  "blog"
+
+  def show
+    @title = "some custom page title"
+  end
+
+
   def post
-  render "/#{self.controller_name}/#{params[:title]}/index.html"
+    render "/#{self.controller_name}/#{params[:title]}/index.html"
   end
 end
